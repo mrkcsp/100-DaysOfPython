766e9cbdfaeb980c7ccfb1cf5360a11db6dbc360
mrkcsp <34489397+mrkcsp@users.noreply.github.com>
2024-12-30 18:47:52 +0100

    Initial commit

---
+++ day-1/index.py
@@ -0,0 +1,5 @@
+print("Creiamo il nome per la tua band!")
+citta_nascita = input("Qual'è il nome della tua città di nascita?  ")
+nome_animale = input("Qual'è il nome del tuo animale domestico?  ")
+
+print(f"Ti consiglio come nome della tua band: {citta_nascita.capitalize()} {nome_animale.capitalize()}, buona fortuna!")
