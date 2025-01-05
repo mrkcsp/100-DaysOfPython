4dd2704d95b9f09569c98dd8175578f8c930f9b3
mrkcsp <34489397+mrkcsp@users.noreply.github.com>
2025-01-05 19:36:54 +0100

    Excercises + escape the maze mini game
    Need to re-check a part of the code to completely understand how that part works better than do it in the second "while"

---
+++ Day6-EscapeTheMaze/excercises.py
@@ -0,0 +1,31 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+# link:  https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     if front_is_clear():
+#         move()
+#         turn_right()
+#         while front_is_clear():
+#             move()
+#         turn_left()
+        
+        
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+        
