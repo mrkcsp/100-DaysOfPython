34a88ca0d7fb923a50eab62b53ebfccf8375f34a
mrkcsp <34489397+mrkcsp@users.noreply.github.com>
2025-01-07 14:39:00 +0100

    Update hangman game - resolved some bug issues
    Resolved:
    -Control for already tried characters is now working.
    -Misunderstanding of .isalpha() has been clarified.
    -Corrected if statements to elif to ensure the program stops where needed.

--- Day7-Hangman/index.py
+++ Day7-Hangman/index.py
@@ -21,18 +21,21 @@ def hangaman(words):
     while lifes > 0 and "_" in guessed_word:
         print(f"The word to guess is {guessed_word}")
         user_char_input = input("Insert a char to check:\n")
-        if len(user_char_input) > 1 and user_char_input.isalpha():
+        if len(user_char_input) > 1:
             print("Insert a unique valid char please")
         
-        if user_char_input in guessed_letters:
+        elif user_char_input in guessed_letters:
             print(f"You already tried '{user_char_input}' char")
 
-        if user_char_input in word_to_find:
+        elif user_char_input in word_to_find:
+            guessed_letters.append(user_char_input)
             print(f"Yes! The '{user_char_input}' is in the word!")
+            print()
             for i, char in enumerate(word_to_find):
                 if char == user_char_input:
                     guessed_word[i] = user_char_input
         else:
+            guessed_letters.append(user_char_input)
             lifes -= 1
             print(f"Oh no! The '{user_char_input}' is not in the word :(")
             print(f"######## lifes {lifes}/6 ########")
