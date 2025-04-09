aafe76ce5e032cdaa001c3724b675e3db0f01b95
mrkcsp <mrkcsp94@hotmail.it>
2025-04-09 20:15:01 +0200

    turtle race, bet on ur favourite turtle!

---
+++ Day19-Sketch/turtleRace.py
@@ -0,0 +1,47 @@
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(width=500, height=400)
+
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+
+colors = ["red", "yellow", "orange", "green", "purple", "blue"]
+
+startY = -120
+
+is_race_on = False
+
+all_turtles = []
+
+for turtle_index in range(0, 6):
+    if(turtle_index == 3):
+        startY +=20 
+        print("aggiustato")
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=startY)
+    startY += 40
+    all_turtles.append(new_turtle)
+
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else: 
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+
+
+screen.exitonclick()
