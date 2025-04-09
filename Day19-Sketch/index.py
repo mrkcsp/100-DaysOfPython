80b90643806d1c7c4d4290b73f3f6fefd4016a77
mrkcsp <mrkcsp94@hotmail.it>
2025-04-09 20:14:53 +0200

    completed the excercise on turtle movement

--- Day19-Sketch/index.py
+++ Day19-Sketch/index.py
@@ -6,8 +6,24 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+def move_backwards():
+    tim.backward(10)
+
+def rotate_left():
+    tim.left(10)
+
+def rotate_right():
+    tim.right(10)
+
+
 
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(fun=move_forwards, key="w")
+screen.onkey(fun=move_backwards, key="s")
+screen.onkey(fun=rotate_left, key="a")
+screen.onkey(fun=rotate_right, key="d")
+screen.onkey(fun=tim.clear, key="c")
+
+
 
 screen.exitonclick()
