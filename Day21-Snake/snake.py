a934bf48f454781f668a20bb592d730bf93aa5aa
mrkcsp <34489397+mrkcsp@users.noreply.github.com>
2025-04-18 17:00:12 +0200

    Removed a space

--- Day21-Snake/snake.py
+++ Day21-Snake/snake.py
@@ -14,7 +14,6 @@ class Snake:
         self.create_snake()
         self.head = self.segments[0]
 
-
     def create_snake(self):
         for position in STARTING_POSITIONS:
             self.add_segment(position)
